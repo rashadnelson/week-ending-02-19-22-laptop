17bedecc0149d2fea1294ba914e6ad61484ef9c6
Rashad Nelson <rashadnelson@gmail.com>
2022-02-18 19:44:43 -0500

    CodeWars - 02/18/22

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 function reverseString(str) {
     // Step 1. Use the split() method to return a new array
-    var splitString = str.split(""); // var splitString = "hello".split("");
-    // ["h", "e", "l", "l", "o"]
+    var splitString = str.split(""); 
  
     // Step 2. Use the reverse() method to reverse the new created array
-    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-    // ["o", "l", "l", "e", "h"]
+    var reverseArray = splitString.reverse(); 
  
     // Step 3. Use the join() method to join all elements of the array into a string
-    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-    // "olleh"
+    var joinArray = reverseArray.join(""); 
     
     //Step 4. Return the reversed string
-    return joinArray; // "olleh"
+    return joinArray; // 
 }
  
-reverseString("hello");
+reverseString("good day");
